0a70500461e9eb741c2ca284dd1e28087afecf9d
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2024-03-26 07:10:35 +0200

    fix filterbyid ru

--- controllers/shop/getShopById_ru.js
+++ controllers/shop/getShopById_ru.js
@@ -1,13 +1,13 @@
-const { Shop_ua } = require('../../models');
+const { Shop_ru } = require('../../models');
 
-const getShopById_ua = async (req, res, next) => {
+const getShopById_ru = async (req, res, next) => {
   const id = req.params.id;
   try {
-    const shop = await Shop_ua.find({ article: id });
+    const shop = await Shop_ru.find({ article: id });
     res.status(200).json(shop);
   } catch (error) {
     throw new WrongIdError(error.message);
   }
 };
 
-module.exports = getShopById_ua;
+module.exports = getShopById_ru;
