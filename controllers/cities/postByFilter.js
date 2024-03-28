5a519810e9676cf9f05153ef8666f000aa846caa
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2024-03-28 14:23:38 +0200

    add ciities for NP

--- controllers/cities/postByFilter.js
+++ controllers/cities/postByFilter.js
@@ -1,13 +1,43 @@
-const { CitiesNP } = require("../../models");
+// const { CitiesNP } = require("../../models");
+const axios = require("axios");
 
 const postByFilter = async (req, res, next) => {
   const filter = req.body.filter;
+
+  const customHeaders = {
+    "Content-Type": "application/json, text/plain, */*",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,POST,PUT,PATCH,DELETE,OPTIONS",
+    "Access-control-allow-headers":
+      "Origin,authorization,Access-Control-Allow-Origin,Content-Type,SOAPAction",
+    "Cache-control": "max-age=43200, public, must-revalidate, proxy-revalidate",
+  };
+
   if (filter) {
     try {
-      const cities = await CitiesNP.find({
-        Description: { $regex: `${filter}`, $options: "i" },
-      });
-      res.status(200).json(cities);
+      axios({
+        method: 'post',
+        url: `https://api.novaposhta.ua/v2.0/json/`,
+        headers: customHeaders, 
+        data: {
+          apiKey: process.env.NovaPoshta_KEY,
+          modelName: "Address",
+          calledMethod: "getSettlements",
+          Warehouse: 1,
+          methodProperties: {
+            Warehouse: "1",
+            FindByString: filter,
+            Limit: "30",
+          },
+        },
+      })
+        .then((data) => {
+          console.log(data.data);
+          res.status(200).json(data.data);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
     } catch (error) {
       res.status(400).json({ message: "Invalid filters characters" });
     }
